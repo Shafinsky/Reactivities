3d38394effd5a2299b0345dda4e5ff8e9842400e
Shafinsky <shafyarik@gmail.com>
2026-02-09 22:25:28 +0200

    add React Router and configure client navigation

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,51 +1,14 @@
-import { Box, Container, CssBaseline, Typography} from "@mui/material";
-import { useState } from "react"
+import { Box, Container, CssBaseline} from "@mui/material";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useActivities } from "../../lib/hooks/useActivities";
-
+import { Outlet } from "react-router";
 
 function App() {
-  
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const {activities, isPending} = useActivities();
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities!.find(x => x.id === id));
-  }
-
-  const handlecancelActivity = () => {
-    setSelectedActivity(undefined);
-  }
-
-  const handleopenForm = (id? : string) => {
-    if (id) handleSelectActivity(id);
-    else handlecancelActivity();
-    setEditMode(true);
-  }
-  const handlecloseForm = () => {
-    setEditMode(false);
-  }
-
   return (
     <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh' }}>
       <CssBaseline />
-      <NavBar openForm={handleopenForm} />
+      <NavBar />
       <Container maxWidth='xl' sx={{mt: 3}}>
-        {!activities || isPending ? (
-          <Typography>Loading...</Typography>
-        ) : (
-          <ActivityDashboard 
-            activities={activities} 
-            selectActivity={handleSelectActivity}
-            cancelSelectActivity={handlecancelActivity}
-            selectedActivity={selectedActivity}
-            editMode={editMode}
-            openForm={handleopenForm}
-            closeForm={handlecloseForm}
-         />
-        )}
+        <Outlet />
       </Container>
     </Box>
   )
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,10 +1,9 @@
 import { Group } from "@mui/icons-material";
-import { Box, AppBar, Toolbar, Typography, Button, Container, MenuItem } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Container, MenuItem } from "@mui/material";
+import { NavLink } from "react-router";
+import MenuItemLink from "../shared/components/MenuItemLink";
 
-type Props = {
-    openForm: () => void
-}
-export default function NavBar({openForm}: Props) {
+export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{
@@ -13,36 +12,22 @@ export default function NavBar({openForm}: Props) {
             <Container maxWidth='xl'>
                 <Toolbar sx={{dispaly: 'flex', justifyContent: 'space-between'}}>
                     <Box>
-                        <MenuItem sx={{dispaly: 'flex', gap: 2}}>
+                        <MenuItem component={NavLink} to='/' sx={{dispaly: 'flex', gap: 2}}>
                             <Group fontSize="large" />
                             <Typography variant="h4" fontWeight='bolt'>Reactivities</Typography>
                         </MenuItem>
                     </Box>
                     <Box sx={{display: 'flex'}}>
-                        <MenuItem sx={{
-                            fontSize: '1.2 rem', textTransform: 'uppercase', fontWeight: 'bold'
-                        }}>
+                        <MenuItemLink to='/activities'>
                             Activities
-                        </MenuItem>
-                        <MenuItem sx={{
-                            fontSize: '1.2 rem', textTransform: 'uppercase', fontWeight: 'bold'
-                        }}>
-                            About
-                        </MenuItem>
-                        <MenuItem sx={{
-                            fontSize: '1.2 rem', textTransform: 'uppercase', fontWeight: 'bold'
-                        }}>
-                            Contact
-                        </MenuItem>
+                        </MenuItemLink>
+                        <MenuItemLink to='/createActivity'>
+                            Create Activity
+                        </MenuItemLink>
                     </Box>
-                    <Button 
-                        size="large" 
-                        variant="contained" 
-                        color="warning"
-                        onClick={openForm}
-                    >
-                        Create activity
-                    </Button>
+                    <MenuItem>
+                        User Menu
+                    </MenuItem>
                 </Toolbar>
             </Container>
         </AppBar>
