272a9291d49e888f5324aa0e0834a35bda0ff81a
Shafinsky <shafyarik@gmail.com>
2026-02-14 15:05:18 +0200

    Section 9 - introduce client state, MobX, Mobx react Lite, React Context

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,36 +1,60 @@
 import { Group } from "@mui/icons-material";
-import { Box, AppBar, Toolbar, Typography, Container, MenuItem } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Container, MenuItem, LinearProgress } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 export default function NavBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{
-            backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-        }}>
-            <Container maxWidth='xl'>
-                <Toolbar sx={{dispaly: 'flex', justifyContent: 'space-between'}}>
-                    <Box>
-                        <MenuItem component={NavLink} to='/' sx={{dispaly: 'flex', gap: 2}}>
-                            <Group fontSize="large" />
-                            <Typography variant="h4" fontWeight='bolt'>Reactivities</Typography>
+    const {uiStore} = useStore();
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{
+                backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+                position: 'relative'
+            }}>
+                <Container maxWidth='xl'>
+                    <Toolbar sx={{dispaly: 'flex', justifyContent: 'space-between'}}>
+                        <Box>
+                            <MenuItem component={NavLink} to='/' sx={{dispaly: 'flex', gap: 2}}>
+                                <Group fontSize="large" />
+                                <Typography variant="h4" fontWeight='bolt'>Reactivities</Typography>
+                            </MenuItem>
+                        </Box>
+                        <Box sx={{display: 'flex'}}>
+                            <MenuItemLink to='/activities'>
+                                Activities
+                            </MenuItemLink>
+                            <MenuItemLink to='/createActivity'>
+                                Create Activity
+                            </MenuItemLink>
+                            <MenuItemLink to='/counter'>
+                                Counter
+                            </MenuItemLink>
+                        </Box>
+                        <MenuItem>
+                            User Menu
                         </MenuItem>
-                    </Box>
-                    <Box sx={{display: 'flex'}}>
-                        <MenuItemLink to='/activities'>
-                            Activities
-                        </MenuItemLink>
-                        <MenuItemLink to='/createActivity'>
-                            Create Activity
-                        </MenuItemLink>
-                    </Box>
-                    <MenuItem>
-                        User Menu
-                    </MenuItem>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    </Box>
+                    </Toolbar>
+                </Container>
+
+                <Observer>
+                    {() => uiStore.isLoading ? (
+                        <LinearProgress
+                            color="secondary"
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                height: 4
+                            }}
+                        />
+                    ) : null}
+                </Observer>
+
+            </AppBar>
+        </Box>
     )
 }
