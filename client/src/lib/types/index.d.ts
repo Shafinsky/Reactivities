7485c04cc58c7148bbe026950ef936003888dade
Shafinsky <shafyarik@gmail.com>
2026-02-23 14:00:56 +0200

    implemented ActivityForm with react-hook-form + zod validation, add LocationIQ autocomplete, creating reusable form inputs

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -2,11 +2,41 @@ type Activity = {
   id: string
   title: string
   description: string
-  date: string
+  date: Date
   category: string
   isCancelled: boolean
   city: string
   venue: string
   latitude: number
   longitude: number
+}
+
+export type LocationIQSuggestion = {
+  place_id: string
+  osm_id: string
+  osm_type: string
+  licence: string
+  lat: string
+  lon: string
+  boundingbox: string[]
+  class: string
+  type: string
+  display_name: string
+  display_place: string
+  display_address: string
+  address: LocationIQAddress
+}
+
+export type LocationIQAddress = {
+  name: string
+  house_number: string
+  road: string
+  suburb?: string
+  town?: string
+  village?: string
+  city?: string
+  state: string
+  postcode: string
+  country: string
+  country_code: string
 }
