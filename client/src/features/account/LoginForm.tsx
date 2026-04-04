8a92156533bd814135159984651203868da7f115
Shafinsky <shafyarik@gmail.com>
2026-04-04 15:17:32 +0300

    Email confirmation complete

--- client/src/features/account/LoginForm.tsx
+++ client/src/features/account/LoginForm.tsx
@@ -6,29 +6,49 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 import { LockOpen } from "@mui/icons-material";
 import TextInput from "../../app/shared/components/TextInput";
 import { Link, useLocation, useNavigate } from "react-router";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
-    const { loginUser } = useAccount();
+    const [notVerified, setNotVerified] = useState(false);
+    const { loginUser, resendConfirmationEmail } = useAccount();
     const navigate = useNavigate();
     const location = useLocation();
-    const { control, handleSubmit, formState: {isValid, isSubmitting} } = useForm<LoginSchema>({
+    const { control, handleSubmit, watch, formState: { isValid, isSubmitting } } = useForm<LoginSchema>({
         mode: 'onTouched',
         resolver: zodResolver(loginSchema)
     });
+    
+    const email = watch('email');
+
+    const handleResendEmail = async () => {
+        try {
+            await resendConfirmationEmail.mutateAsync({email});
+            setNotVerified(false);
+        } catch (error) {
+            console.log(error);
+            toast.error('Problem sending email - please check email address');
+        }
+    }
 
     const onSubmit = async (data: LoginSchema) => {
         await loginUser.mutateAsync(data, {
             onSuccess: () => {
                 navigate(location.state?.from || '/activities');
+            },
+            onError: error => {
+                if (error.message === 'NotAllowed') {
+                    setNotVerified(true);
+                }
             }
         });
     }
 
     return (
-        <Paper 
-            component='form' 
+        <Paper
+            component='form'
             onSubmit={handleSubmit(onSubmit)}
-            sx = {{
+            sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 p: 3,
@@ -38,13 +58,13 @@ export default function LoginForm() {
                 borderRadius: 3
             }}
         >
-            <Box display='flex' alignItems='center' justifyContent='center' 
+            <Box display='flex' alignItems='center' justifyContent='center'
                 gap={3} color='secondary.main'>
                 <LockOpen fontSize="large" />
                 <Typography variant="h4">Sign in</Typography>
             </Box>
-            <TextInput label='Email' control={control} name='email'/>
-            <TextInput label='Password' type='password' control={control} name='password'/>
+            <TextInput label='Email' control={control} name='email' />
+            <TextInput label='Password' type='password' control={control} name='password' />
             <Button
                 type='submit'
                 disabled={!isValid || isSubmitting}
@@ -53,12 +73,26 @@ export default function LoginForm() {
             >
                 Login
             </Button>
-            <Typography sx={{textAlign: 'center'}}>
-                Don't have an account?
-                <Typography sx={{ml: 2}} component={Link} to='/register' color='primary'>
-                    Sign up
+            {notVerified ? (
+                <Box display='flex' flexDirection='column' alignItems='center'>
+                    <Typography textAlign='center' color='error'>
+                        Your email has not been verified. You can click the button to re-send the verfication link.
+                    </Typography>
+                    <Button
+                        disabled={resendConfirmationEmail.isPending}
+                        onClick={handleResendEmail}
+                    >
+                        Re-send email link
+                    </Button>
+                </Box>
+            ) : (
+                <Typography sx={{ textAlign: 'center' }}>
+                    Don't have an account?
+                    <Typography sx={{ ml: 2 }} component={Link} to='/register' color='primary'>
+                        Sign up
+                    </Typography>
                 </Typography>
-            </Typography>
+            )}
         </Paper>
     )
 }
